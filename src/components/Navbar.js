01c3e459f7ef6acb61ad4c018ff385ce8b991770
Angie <lachristie.2021@scis.smu.edu.sg>
2023-08-26 18:59:36 +0800

    ui-fix: changed logout button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,86 @@
-import { Link } from "react-router-dom"
-import { useAuthContext } from "../hooks/useAuthContext"
-import { useLogout } from '../hooks/useLogout'
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useLogout } from "../hooks/useLogout";
 
 // styles
-import styles from './Navbar.module.css'
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
-  const { logout } = useLogout()
-  const { user } = useAuthContext()
-  console.log(user)
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
+  console.log(user);
+
+  const [buttonText, setButtonText] = useState("Welcome, "+(user ? user.displayName : ""));
+  const buttonRef = useRef(null);
+
+  useEffect(() => {
+    if (user) {
+      setButtonText("Welcome, "+user.displayName);
+    } else {
+      setButtonText("Login");
+    }
+
+    if (buttonRef.current) {
+      const buttonWidth = buttonRef.current.getBoundingClientRect().width;
+      buttonRef.current.style.width = buttonWidth + "px";
+    }
+  }, [user]);
+  
+  const handleMouseOver = () => {
+    if (user) {
+      setButtonText("Logout");
+    } else {
+      setButtonText("Login");
+    }
+  };
+
+  const handleMouseOut = () => {
+    if (user) {
+      setButtonText("Welcome, " + user.displayName);
+    } else {
+      setButtonText("Login");
+    }
+  };
 
   return (
     <nav className={styles.navbar}>
       <ul>
-        <li className={styles.title}>myMoney</li>
-        
+        <li className={styles.title}>TaskMaster</li>
+
         {!user && (
           <>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/signup">Signup</Link></li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
           </>
         )}
 
         {user && (
           <>
-            <li>hello, {user.displayName}</li>
-            <li><Link to="/friends">Friends</Link></li>
             <li>
-              <button className="btn" onClick={logout}>Logout</button>
+              Energy <button className="btn">100 pts</button>
+            </li>
+            <li>
+              <Link to="/friends">Friends</Link>
+            </li>
+            <li>
+              <button
+                className="btn welcome-btn"
+                onClick={logout}
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
+                ref={buttonRef}
+              >
+                {buttonText}
+              </button>
             </li>
           </>
         )}
       </ul>
     </nav>
-  )
+  );
 }
